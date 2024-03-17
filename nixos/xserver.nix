eb7490418a45c63530ae8c655d9e1ffd2079eda8
Onekki <285079772@qq.com>
2024-03-17 21:41:16 +0800

    update xserver

--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-terminal
+    gnome.nautilus
+  ];
+  environment.variables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "2";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
   services.xserver = {
     enable = true;
     displayManager = {
@@ -7,5 +16,9 @@
         pkgs.gnome.gnome-session.sessions
       ];
     };
+    dpi = 180;
+    excludePackages = with pkgs; [
+      xterm
+    ];
   };
 }
