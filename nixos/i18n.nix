99e04f3d78e1b9dd3f77b55fa6be0709584dab8a
Onekki <285079772@qq.com>
2024-03-25 23:53:18 +0800

    update fonts

--- nixos/i18n.nix
+++ nixos/i18n.nix
@@ -16,14 +16,18 @@
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-      sarasa-gothic
+      source-han-mono
+      source-han-sans
+      source-han-serif
+      noto-fonts-color-emoji
     ];
 
     fontconfig = {
       defaultFonts = {
-        serif = [ "Sarasa Fixed Slab SC" ];
-        sansSerif = [ "Sarasa Fixed SC" ];
-        monospace = [ "Sarasa Mono SC" ];
+        emoji = [ "Noto Color Emoji" ];
+        serif = [ "Source Han Serif SC" ];
+        sansSerif = [ "Source Han Sans SC" ];
+        monospace = [ "Source Han Mono SC" ];
       };
     };
   };
